4002265322c04a9375dea6d3f605a3a4c6900731
Francisco Racciatti <mokenuf@me.com>
2024-01-07 12:04:43 -0300

    Add-Prettier
    Added Prettier to the project.

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,20 +1,20 @@
 <template>
-  <nav class="bg-slate-900 text-white p-4 flex items-center">
-    <button v-if="showBackIcon" @click="goBack" class="mr-1">
-      <span>&lt;</span>
-    </button>
-    HAIR RECOVERY - WOW GUILD
-  </nav>
+	<nav class="bg-slate-900 text-white p-4 flex items-center">
+		<button v-if="showBackIcon" @click="goBack" class="mr-1">
+			<span>&lt;</span>
+		</button>
+		HAIR RECOVERY - WOW GUILD
+	</nav>
 </template>
 <script setup lang="ts">
-import { useRoute, useRouter } from "vue-router";
+import { useRoute, useRouter } from 'vue-router'
 
-const route = useRoute();
-const router = useRouter();
+const route = useRoute()
+const router = useRouter()
 
-const showBackIcon = computed(() => route.path !== "/");
+const showBackIcon = computed(() => route.path !== '/')
 
 function goBack() {
-  router.go(-1);
+	router.go(-1)
 }
 </script>
